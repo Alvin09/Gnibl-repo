60d68c59dd6a633c4a6a9e8f8c1b8178b50ab91e
Alvin <alvin.katt09@gmail.com>
2013-03-10 10:59:05 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gnibl::Application.config.secret_token = 'f3dc43278cf6fddd5140531dd2280021b0151fd77f6b55d1ed11004cc2534de9ffbfb715125020d6a161ee424c5b62c2e94ece18f8aa4a70e797ee32efe4da57'
